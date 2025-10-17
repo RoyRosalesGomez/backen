4bcd7ba57cf37c946b0bfd968eb3b926061d7c85
ROY ROSALES GOMEZ <roy.rosales.gomez@est.una.ac.cr>
2025-10-16 22:55:19 -0600

    Config de app.module para base de datos compartida

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,13 +18,13 @@ import { AppController } from './app.controller';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 3307,
+      host: process.env.DB_HOST || 'interchange.proxy.rlwy.net',
+      port: parseInt(process.env.DB_PORT) || 31113,
       username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_DATABASE || 'agroglobal',
+      password: process.env.DB_PASSWORD || 'zQyxNCmCIFAYWyewsWjWACmLMxsNYRXM',
+      database: process.env.DB_DATABASE || 'railway',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false, // Solo para desarrollo
+      synchronize: true, // Solo para desarrollo
       logging: true,
     }),
     AuthModule,
