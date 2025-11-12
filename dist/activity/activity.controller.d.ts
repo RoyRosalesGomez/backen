870ee6792a44d02a5c3c48e8ca754acfe6535d58
arielcamposchavarria <campos2004.05@gmail.com>
2025-11-11 20:32:41 -0600

    fix-images

--- dist/activity/activity.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ActivityService } from './activity.service';
-export declare class ActivityController {
-    private readonly activities;
-    constructor(activities: ActivityService);
-    list(sinceDays?: string, limit?: string): Promise<import("./activity.entity").Activity[]>;
-}
--- dist/cultivos/dto/create-cultivo.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateCultivoDto {
-    name: string;
-    variedad: string;
-    comentario?: string;
-    image?: string;
-    farmerId?: number;
-}
